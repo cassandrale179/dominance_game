dfb2a78615f5d49dde3a94f746e7ceb2c2300939
Cassandra Le <lenguyetminh1998@gmail.com>
2018-11-16 13:35:40 -0500

    DONE: set up server with webpack

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import 'phaser';
+import pkg from '../package.json';
+
+const width = 1420;
+const height = 800;
+
+const config = {
+    width,
+    height,
+    type: Phaser.AUTO,
+    scene: { create }
+};
+
+const game = new Phaser.Game(config); 
+
+function create() {
+    const welcomeMessage = `Phaser Dev Starter`;
+    
+
+    this.add
+        .text(10, 10, welcomeMessage, { font: "bold 24px Raleway", fill: "#ffff00" });
+
+    this.add
+        .text(width - 50, height - 30, `v${pkg.version}`, { font: "bold 14px Raleway", fill: "#ffff00" });
+}
