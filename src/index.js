13ece9ae2d4628db3d92d8c76c6d857c6cf5e77c
Cassandra Le <lenguyetminh1998@gmail.com>
2018-11-16 13:47:34 -0500

    Game screen

--- src/index.js
+++ src/index.js
@@ -8,17 +8,24 @@ const config = {
     width,
     height,
     type: Phaser.AUTO,
-    scene: { create }
+    scene: { preload, create }
 };
 
 const game = new Phaser.Game(config); 
 
+function preload(){
+    this.load.image('background', 'assets/background4.png'); 
+}
+
+
+// Main funciton of the game screen here 
 function create() {
-    const welcomeMessage = `Phaser Dev Starter`;
+    const welcomeMessage = `Game Screen`;
+    this.add.image(300 , 400,  'background'); 
     
 
     this.add
-        .text(10, 10, welcomeMessage, { font: "bold 24px Raleway", fill: "#ffff00" });
+        .text(10, 10, welcomeMessage, { font: "bold 24px Raleway", fill: "#00000" });
 
     this.add
         .text(width - 50, height - 30, `v${pkg.version}`, { font: "bold 14px Raleway", fill: "#ffff00" });
